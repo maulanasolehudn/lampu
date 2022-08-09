57a45ac99bf552553e60b10234e0ab8aecbda51a
maulanasolehudn <solehudinmaulana75@gmail.com>
2022-08-09 11:08:45 +0700

    lampu

---
+++ main.js
@@ -0,0 +1,15 @@
+function clRed (){
+    document.getElementById("red").style.background="red";
+    document.getElementById("yellow").style.background="rgb(136, 136, 12)";
+    document.getElementById("green").style.background="rgb(0, 94, 0)";
+}
+function clYellow (){
+    document.getElementById("yellow").style.background="yellow";
+    document.getElementById("red").style.background="rgb(182, 7, 7)";
+    document.getElementById("green").style.background="rgb(0, 94, 0)";
+}
+function clGreen (){
+    document.getElementById("green").style.background="lime";
+    document.getElementById("red").style.background="rgb(182, 7, 7)";
+    document.getElementById("yellow").style.background="rgb(136, 136, 12)";
+}
